06c6784bcb579c088e10320ad31dbe3b7e03cd94
solmead <solmead@gmail.com>
2013-04-06 20:06:12 -0400

    Initial
    Initial Commit

---
+++ LinqDynamic/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LinqDynamic")> 
+<Assembly: AssemblyDescription("Allows string querying of iQueryable objects, and turning List into Datatables and back.")> 
+<Assembly: AssemblyCompany("Solmead Productions")> 
+<Assembly: AssemblyProduct("LinqDynamic")> 
+<Assembly: AssemblyCopyright("Copyright © 2012 Solmead Productions")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("abb83084-4382-46a8-9da4-5c4f698f5f89")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.8")> 
+<Assembly: AssemblyFileVersion("1.8")> 
